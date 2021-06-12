aa5248a31f6b0caba5648a4fe28ef49c88e555de
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-06-12 00:57:24 -0500

    📦 NEW: Add eslint and prettier configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const chalk = require('chalk')
-const app = express()
-const port = 3000
-const log = console.log
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+const port = 3000;
+const log = console.log;
+
+const a = 0;
 
 app.get('/', (req, res) => {
-  res.send('Hello World 🦙!')
-})
+  res.send('Hello World 🦙!');
+});
 
 app.listen(port, () => {
-    log(
-        chalk.yellowBright.bgBlack.bold(
-          `Server running at http://${chalk.green('localhost')}:${chalk.red(port)}/`,
-        )
-    );
-})
+  log(
+    chalk.yellowBright.bgBlack.bold(
+      `Server running at http://${chalk.green('localhost')}:${chalk.red(
+        port,
+      )}/`,
+    ),
+  );
+});
