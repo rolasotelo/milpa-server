7e59faffba38ee1502501c2dbae3f903ec7df5cf
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-06-11 23:58:02 -0500

    📦 NEW: Add express hello world app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const chalk = require('chalk')
+const app = express()
+const port = 3000
+const log = console.log
+
+app.get('/', (req, res) => {
+  res.send('Hello World 🦙!')
+})
+
+app.listen(port, () => {
+    log(
+        chalk.yellowBright.bgBlack.bold(
+          `Server running at http://${chalk.green('localhost')}:${chalk.red(port)}/`,
+        )
+    );
+})
