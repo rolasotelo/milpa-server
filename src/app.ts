a57a8f41419991be4c0a4e8213d0db4327e43326
Rolando Sotelo <rolando.s.alarcon@icloud.com>
2021-08-19 16:24:58 -0500

    📦 NEW: emit start game on MAX_PLAYERS

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import chalk from 'chalk';
+import chalk, { italic } from 'chalk';
 import { Server as HttpServer } from 'http';
 import { Server } from 'socket.io';
+import { MAX_PLAYERS } from './common/constants';
 import { MiRemoteSocket, MiSocket } from './common/types/types';
 
 const log = console.log;
@@ -40,7 +41,7 @@ export function createApplication(httpServer: HttpServer): Server {
         });
       }
 
-      if (sockets.length < 2) {
+      if (sockets.length < MAX_PLAYERS) {
         socket.join(roomCode);
 
         users.push({
@@ -56,13 +57,19 @@ export function createApplication(httpServer: HttpServer): Server {
           userID: socket.id,
           nickname: socket.nickname,
         });
+
+        socket.emit('users', users);
+
+        if (users.length === MAX_PLAYERS) {
+          io.to(roomCode).emit('start game');
+        }
       } else {
         socket.in(roomCode).emit('connection attempted', {
           userID: socket.id,
           nickname: socket.nickname,
         });
+        socket.emit('room filled');
       }
-      socket.emit('users', users);
     } else {
       // ? what should I do if there is no gameCode in the handshake
     }
